f288cc881d2cf70d106ccb1bf60d8336185cedeb
Senzo <48803422+Senzo-crypto@users.noreply.github.com>
2022-12-28 21:40:42 -0800

    UI/UX
    Edited starter template to my  liking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import Head from "next/head";
 import { CardanoWallet, MeshBadge } from "@meshsdk/react";
 
+const betAmounts = [1, 5, 10];
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Mesh App on Cardano</title>
         <meta name="description" content="A Cardano dApp powered my Mesh" />
-        <link
-          rel="icon"
-          href="https://meshjs.dev/favicon/favicon-32x32.png"
-        />
+        <link rel="icon" href="https://meshjs.dev/favicon/favicon-32x32.png" />
         <link
           href="https://meshjs.dev/css/template.css"
           rel="stylesheet"
@@ -20,40 +19,24 @@ export default function Home() {
 
       <main className="main">
         <h1 className="title">
-          <a href="https://meshjs.dev/">Mesh</a> Next.js
+          <a href="https://cardanocoinflippers.com/" className="LinkSite">
+            Cardano
+          </a>{" "}
+          Coin Flippers
+          <p className="Description">Flipping made easy</p>
         </h1>
 
         <div className="demo">
           <CardanoWallet />
         </div>
-
-        <div className="grid">
-          <a href="https://meshjs.dev/apis" className="card">
-            <h2>Documentation</h2>
-            <p>
-              Our documentation provide live demos and code samples; great
-              educational tool for learning how Cardano works.
-            </p>
-          </a>
-
-          <a href="https://meshjs.dev/guides" className="card">
-            <h2>Guides</h2>
-            <p>
-              Whether you are launching a new NFT project or ecommerce store,
-              these guides will help you get started.
-            </p>
-          </a>
-
-          <a href="https://meshjs.dev/react" className="card">
-            <h2>React components</h2>
-            <p>
-              Useful React UI components and hooks, seamlessly integrate them
-              into your app, and bring the user interface to life.
-            </p>
-          </a>
+        <div className="BetAmount-container">
+          <div className="BetAmount">
+            {betAmounts.map((amount) => (
+              <button key={amount}>{amount} Ada</button>
+            ))}
+          </div>
         </div>
       </main>
-
       <footer className="footer">
         <MeshBadge dark={true} />
       </footer>
