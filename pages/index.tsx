6dbeac852eba9d69d1dc94c858b83ec5e9947200
Senzo <48803422+Senzo-crypto@users.noreply.github.com>
2023-01-01 18:19:42 -0800

    Added  contract  file
    Added  a place holder for an example  smart contract file, this will be replaced with the real one later

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,49 @@
 import Head from "next/head";
-import { CardanoWallet, MeshBadge } from "@meshsdk/react";
-import { Lucid } from "lucid-cardano"
+import { CardanoWallet, MeshBadge, useWallet } from "@meshsdk/react";
+import { Lucid } from "lucid-cardano";
+import { script, scriptAddr } from "../config/contract";
+import {
+  Transaction,
+  Data,
+  BlockfrostProvider,
+  resolveDataHash,
+} from "@meshsdk/core";
+import { useState } from "react";
+import type { NextPage } from "next";
 
 const lucid = await Lucid.new();
 
 const betAmounts = [1, 5, 10];
 
-export default function Home() {
+const Home: NextPage = () => {
+  const { wallet, connected, connecting } = useWallet();
+  const [loading, setLoading] = useState<boolean>(false);
+  async function lockFunds() {
+    if (wallet) {
+      const addr = (await wallet.getUsedAddresses())[0];
+      const d: Data = {
+        alternative: 0,
+        fields: [42],
+      };
+      const tx = new Transaction({ initiator: wallet }).sendAssets(
+        {
+          address: scriptAddr,
+          datum: {
+            value: d,
+          },
+        },
+        [
+          {
+            unit: "ce982ed35e3d6319fa62777e6b4145e314d16575cbb79d7532124be57453656e7a6f636f696e",
+            quantity: "1",
+          },
+        ]
+      );
+      const unsignedTx = await tx.build();
+      const signedTx = await wallet.signTx(unsignedTx);
+      const txHash = await wallet.submitTx(signedTx);
+    }
+  }
   return (
     <div className="container">
       <Head>
@@ -39,10 +76,22 @@ export default function Home() {
             ))}
           </div>
         </div>
+        <button
+          type="button"
+          onClick={() => lockFunds()}
+          disabled={connecting || loading}
+          style={{
+            margin: "8px",
+            backgroundColor: connecting || loading ? "orange" : "grey",
+          }}
+        >
+          Lock funds
+        </button>
       </main>
       <footer className="footer">
         <MeshBadge dark={true} />
       </footer>
     </div>
   );
-}
+};
+export default Home;
